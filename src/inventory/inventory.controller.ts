3b2b6d98a22fdb15a5fec9ef195f25f421842401
linda <lnouigui@gmail.com>
2025-04-01 20:42:24 +0100

    inventory

--- src/inventory/inventory.controller.ts
+++ src/inventory/inventory.controller.ts
@@ -1,20 +1,26 @@
-import { Body, Controller, Param, Post } from '@nestjs/common';
+import { Body, Controller, Param, Post, UseGuards } from '@nestjs/common';
 import { InventoryService } from './inventory.service';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateInventoryDto } from './types/dto/create-inventory.dto';
 import { UpdateInventoryDto } from './types/dto/update-inventory.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/auth/guards/roles.decorator';
 
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard, RolesGuard)  
 @ApiTags('inventory Resource')
 @Controller('Inventories')
 export class InventoryController {
     constructor(private readonly inventoryService: InventoryService) {}
 
+    @Roles('admin') 
     @ApiOperation({ summary: 'Launch an inventory' })
     @Post('launch')
     async launchInventory(){
       return this.inventoryService.launchInventory();
     }
-  
+    @Roles('admin') 
     @ApiOperation({ summary: 'Close an inventory' })
     @Post('close')
     async closeInventory() {
