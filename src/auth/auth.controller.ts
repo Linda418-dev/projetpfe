679f26c2aca9adc443608f4122c7a4970da8b5c1
linda <lnouigui@gmail.com>
2025-04-03 18:10:53 +0100

    sign up

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,6 +9,7 @@ import { BypassInventoryLock } from 'src/inventory/guards/bypass-inventory-lock.
 @Controller('Accounts')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+  @BypassInventoryLock() 
   @Post('signup')
   async signup(@Body() createUserDto: CreateUserDto) {
     return this.authService.signup(createUserDto);
