423225ddf7a74b864e8143173a6e54e7fedaaee2
linda <lnouigui@gmail.com>
2025-04-30 12:49:49 +0100

    edit

--- src/anomaly/anomaly.controller.ts
+++ src/anomaly/anomaly.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { AnomalyService } from './anomaly.service';
 import { ApiTags } from '@nestjs/swagger';
 import { CreateAnomalyDto } from './types/dto/create-anomaly.dto';
+import { BypassInventoryLock } from 'src/inventory/guards/bypass-inventory-lock.decorator';
 
 @ApiTags('Anomaly Resource')
 @Controller('anomalies')
@@ -13,26 +14,31 @@ export class AnomalyController {
       return this.anomalyService.getAllanomalies();
     }
 
+    @BypassInventoryLock()
     @Post()
     createAnomaly(@Body() createAnomalyDto: CreateAnomalyDto) {
       return this.anomalyService.createAnomaly(createAnomalyDto);
     }
-  
+    
+    @BypassInventoryLock()
     @Get(':id')
     getAnomalyById(@Param('id') id: string) {
       return this.anomalyService.getAnomalyById(id);
     }
 
+    @BypassInventoryLock()
     @Patch(':id/progress')
     progressAnomaly(@Param('id') id: string){
       return this.anomalyService.progressAnomaly(id);
     }
 
+    @BypassInventoryLock()
     @Patch(':id/accept')
     acceptAnomaly(@Param('id') id: string){
       return this.anomalyService.acceptAnomaly(id);
     }
 
+    @BypassInventoryLock()
     @Patch(':id/refuse')
     refuseAnomaly(@Param('id') id: string){
       return this.anomalyService.refuseAnomaly(id);
